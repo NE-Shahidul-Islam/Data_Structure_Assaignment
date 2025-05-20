6e64f0f629df2760b46e8c083cdb555ba8102371
Shahidul Islam <168478159+NE-Shahidul-Islam@users.noreply.github.com>
2025-05-21 05:09:08 +0600

    Update Q2.cpp

--- 2-1 DS/Graph/Q2.cpp
+++ 2-1 DS/Graph/Q2.cpp
@@ -1,63 +1,44 @@
 #include <iostream>
 using namespace std;
 
-const int MAX = 100;
+#define INF 999999999
+#define MAX 100
 
-void multiply(int A[MAX][MAX], int B[MAX][MAX], int result[MAX][MAX], int m) {
-    for (int i = 0; i < m; ++i)
-        for (int j = 0; j < m; ++j) {
-            result[i][j] = 0;
-            for (int k = 0; k < m; ++k)
-                result[i][j] += A[i][k] * B[k][j];
+void floydWarshall(int dist[MAX][MAX], int n) {
+    for (int k = 0; k < n; k++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+            }
         }
-}
-
-void copy(int from[MAX][MAX], int to[MAX][MAX], int m) {
-    for (int i = 0; i < m; ++i)
-        for (int j = 0; j < m; ++j)
-            to[i][j] = from[i][j];
+    }
 }
 
 int main() {
-    int m, A[MAX][MAX], power[MAX][MAX], temp[MAX][MAX], Br[MAX][MAX] = {0}, path[MAX][MAX];
+    int n;
+    cout << "Enter number of nodes: ";
+    cin >> n;
 
-    cin >> m;
-    for (int i = 0; i < m; ++i)
-        for (int j = 0; j < m; ++j)
-            cin >> A[i][j];
-
-    copy(A, power, m);
-
-    for (int p = 1; p <= m; ++p) {
-        for (int i = 0; i < m; ++i){
-            for (int j = 0; j < m; ++j)
-                Br[i][j] += power[i][j];
-            }
-
-        multiply(power, A, temp, m);
-        copy(temp, power, m);
-    }
-
-    for (int i = 0; i < m; ++i){
-        for (int j = 0; j < m; ++j){
-            if(Br[i][j]) path[i][j]=1;
-            else path[i][j]=0;
+    int dist[MAX][MAX];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> dist[i][j];
+            if(dist[i][j]==0) dist[i][j]=INF;
         }
-     }
-
-    bool stronglyConnected = true;
-    for (int i = 0; i < m && stronglyConnected; ++i)
-        for (int j = 0; j < m && stronglyConnected; ++j)
-            if (path[i][j] == 0)
-                stronglyConnected = false;
-
-    cout << "Path Matrix:\n";
-    for (int i = 0; i < m; ++i) {
-        for (int j = 0; j < m; ++j)
-            cout << path[i][j] << " ";
-        cout << "\n";
     }
 
-    cout << (stronglyConnected ? "Strongly Connected\n" : "Not Strongly Connected\n");
+    floydWarshall(dist, n);
+
+    cout << "\nShortest distance matrix:\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (dist[i][j] == INF)
+                cout << "INF ";
+            else
+                cout << dist[i][j] << " ";
+        }
+        cout << endl;
+    }
+
     return 0;
 }
